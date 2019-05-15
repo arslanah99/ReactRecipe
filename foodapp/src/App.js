f3cb8659616f79e1ed0727d72770b1118d332a7b
arslanah99 <Arah09@hotmail.com>
2019-05-15 15:40:36 -0600

    added styling

--- foodapp/src/App.js
+++ foodapp/src/App.js
@@ -6,33 +6,48 @@ const App = () => {
 
   const APP_ID = '76f9f481'
   const API_KEY = 'c7fc64c5baca02116eb2781c456231d6'
-  const [recipes, setRecipes] = useState([]);
- 
-  const [counter, setCounter] = useState(0);
 
-  useEffect( () => {
-    getRecipes();
-  }, []);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState();
+  const [query, setQuery] = useState('chicken')
+
+
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`)
     const data = await response.json();
     setRecipes(data.hits);
-
     console.log(data.hits)
+  }
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+ 
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
   }
 
   return(
     <div className="App">
-      <form className="search-form">
-        <input type="text" className="search-bar" />
+      <form onSubmit={getSearch} className="search-form">
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
           <button type="submit" className="search-button">
             Search
           </button>
       </form>
+      <div className="recipes">
       {recipes.map(recipe =>(
-        <Recipe />
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
       ))}
+      </div>
     </div>
   )
 }
--- foodapp/src/Recipe.js
+++ foodapp/src/Recipe.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import style from './recipe.module.css';
 
-const Recipe = () => {
+
+const Recipe = ({title, calories, image, ingredients}) => {
     return(
-        <div>
-            <h1>Title</h1>
-            <p>Calories</p>
-            <img src="" alt="" />
-            <h3>Recipe</h3>
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calories}</p>
+            <img src={image} alt="" />
         </div>  
     );
 }
